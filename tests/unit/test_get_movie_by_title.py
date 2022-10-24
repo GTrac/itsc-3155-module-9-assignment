8910ba5abcbf29732bf9a2430d85b2bf5934df26
sean-coyle <scoyle03@outlook.com>
2022-10-23 20:12:59 -0400

    Implemented User Search
    Implemented search_movies() and tests

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,28 @@
 # TODO: Feature 3
+from app import app, create_movie
+from src.models.movie import Movie
+from src.repositories.movie_repository import get_movie_repository
+ 
+def test_get_movie_by_title():
+    test_app = app.test_client()
+    response = test_app.get('/movies/search')
+    assert b'<div class="form-group">'in response.data
+
+
+def test_get_movie_with_None():
+    test_app = app.test_client()
+    response = test_app.get('/movies/search?search=Star+Wars')
+    assert b'Star Wars not found! try a different search' in response.data
+    assert not b'<table class="table">'in response.data
+
+
+def test_get_movie_with_Movie():
+    test_app = app.test_client()
+    response = test_app.get('/movies/search?search=Star+Wars')
+    assert b'One result matching' in response.data
+    assert b'<table class="table">'in response.data
+    assert b'<th scope="col">Star Wars</th>' in response.data
+    assert b'<th scope="col">George Lucas</th>' in response.data
+    assert b'<th scope="col">5</th>' in response.data
+
+    

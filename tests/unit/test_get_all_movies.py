f1979832c515c4b907b3993ed532ce2f8d453823
gtraczyk <traczyk.gilbert@gmail.com>
2022-10-25 15:17:28 -0400

    mostly finished

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,24 @@
 # TODO: Feature 1
+from flask.testing import FlaskClient
+from app import app, create_movie
+from src.models.movie import Movie
+
+def test_display_page():
+    test_app = app.test_client()
+    response = test_app.get('/movies')
+    assert b'<p class="mb-3">See our list of movie ratings below</p>' in response.data
+
+def test_display_movie_title():
+    test_app = app.test_client()
+    response = test_app.get('/movies')
+    assert b'<td id="movie" name = "movie">' in response.data
+
+def test_display_movie_director():
+    test_app = app.test_client()
+    response = test_app.get('/movies')
+    assert b'<td id="director" name = "director">' in response.data
+
+def test_display_movie_rating():
+    test_app = app.test_client()
+    response = test_app.get('/movies')
+    assert b'<td id="rating" name = "rating">' in response.data
